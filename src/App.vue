03b52783b77fe44cc6fda522d1b8c15efe297cbb
kndxhz <kndxhz@163.com>
2025-10-02 15:34:29 +0800

    fix: 更新提示框内容和按钮文本，确保信息准确性

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,10 @@ import 'element-plus/es/components/message-box/style/css';
   const seenMessageBox = await cookieStore.get('seenMessageBox');
   if (seenMessageBox?.value !== 'true') {
     ElMessageBox.alert(
-      "本站确保任何聊天记录不会被篡改、删除<br/>始终保持中立<br/>本站已在GitHub开源（补上链接）<br/>https://github.com/your-repository<br/>如有任何问题，请前往GitHub提交issue",
+      "本站确保任何聊天记录不会被篡改、删除<br/>始终保持中立<br/>本站已在GitHub开源<br/>https://github.com/kndxhz/find-mc-chat<br/>如有任何问题，请前往GitHub提交issue",
       "温馨提示",
       { 
-        confirmButtonText: "我已确认",
+        confirmButtonText: "我已知晓并不再显示",
         dangerouslyUseHTMLString: true
       }
     );
